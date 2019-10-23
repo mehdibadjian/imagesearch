76e9242643587cdbe2a26c8846c676c99fd4f05c
Mehdi Badjian <mmbadjian@gmail.com>
2019-10-23 20:24:24 +0800

    Updates project structure and adds common classes

---
+++ ImageSearch/Sources/Shared/Constants/AppConstants.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherConstants.swift
+//  WeatherApp
+//
+//  Created by Mehdi on 25/9/19.
+//  Copyright © 2019 Mehdi. All rights reserved.
+//
+import Foundation
+enum ApiType: String {
+    case weather
+    case search
+}
+class AppConstants {
+    static var enableMockData: Bool = false
+    //To-Do:- get the API Key from remote
+    static var apiKey: String = "?key=f0b519ce0060402796a40014192509"
+    static var baseAPI: String = "http://api.worldweatheronline.com/premium/v1/"
+    static var atSign: String = "&"
+    static var loaderTag: Int = 1000
+    static var historyKey: String = "history"
+    static var maxHistoryCount: Int = 10
+}
